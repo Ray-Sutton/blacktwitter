f68e002e08ed7551f3493455ea892a95ef910dd9
ray-sutton <rasutton09@gmail.com>
2015-12-02 19:23:38 -0500

    commit:
    tweets/relationship/views/root

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151202234633) do
+ActiveRecord::Schema.define(version: 20151203001147) do
+
+  create_table "tweets", force: :cascade do |t|
+    t.string   "message"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -26,6 +33,10 @@ ActiveRecord::Schema.define(version: 20151202234633) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
+    t.string   "username"
+    t.text     "bio"
+    t.string   "location"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
